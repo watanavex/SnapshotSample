477f80f061a64120320a6cd82edb63e130a731a7
watanave <susan.se3p@gmail.com>
2020-08-11 08:53:38 +0900

    Initial commit

---
+++ SnapshotSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SnapshotSample
+//
+//  Created by Yohta Watanave on 2020/08/11.
+//  Copyright © 2020 Yohta Watanave. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
