fcdc30c206ffa8694b9714a2e216178c512b0058
watanave <susan.se3p@gmail.com>
2020-08-11 10:05:02 +0900

    共通のRecodeModeフラグを定義

---
+++ SnapshotSampleTests/TestCommon.swift
@@ -0,0 +1,11 @@
+//
+//  TestCommon.swift
+//  SnapshotSampleTests
+//
+//  Created by Yohta Watanave on 2020/08/11.
+//  Copyright © 2020 Yohta Watanave. All rights reserved.
+//
+
+import Foundation
+
+let recordMode = false
