bf437a7e0c411bc227b46531bb98acf9e7c0f92e
watanave <susan.se3p@gmail.com>
2020-08-11 09:02:44 +0900

    各画面のViewControllerクラスを追加

---
+++ SnapshotSample/TextViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TextViewController.swift
+//  SnapshotSample
+//
+//  Created by Yohta Watanave on 2020/08/11.
+//  Copyright © 2020 Yohta Watanave. All rights reserved.
+//
+
+import UIKit
+
+class TextViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
